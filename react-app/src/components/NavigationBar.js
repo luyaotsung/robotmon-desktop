249f316f14972ed724ec118b8c9a73a0677b65e1
Shawn <hn62257@gmail.com>
2017-09-30 20:46:52 +0800

    Add Navigation bar

---
+++ react-app/src/components/NavigationBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './NavigationBar.css';
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="nav-title">
+          Robotmon
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NavigationBar;
